e85116bd769deceed5cdd4c010e225dfc29f38a9
Antek <xbbshshxhz@gmail.com>
2023-11-06 16:39:05 +0100

    First Commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,15 +7,15 @@ class CalculatorTest {
     @Test
     @DisplayName("Add two numbers")
     void add() {
-        assertEquals(4, Calculator.add(2, 2));
+        assertEquals(4,Calculator.add(2, 2));
     }
     @Test
     @DisplayName("Multiply two  numbers")
     void multiply() {
-        assertAll(() ->assertEquals(4,Calculator.multiply(2,2)),
-                () ->    assertEquals(-4,Calculator.multiply(2,-2)),
-                () ->    assertEquals(4,Calculator.multiply(-2,-2)),
-                () ->    assertEquals(0,Calculator.multiply(1,0)));
+                assertAll(() ->assertEquals(4,   Calculator.multiply(2,2)),
+                () ->    assertEquals(-4, Calculator.multiply(2,-2)),
+                () ->    assertEquals(4, Calculator.multiply(-2,-2)),
+                () ->    assertEquals(0, Calculator.multiply(1,0)));
     }
 
 
