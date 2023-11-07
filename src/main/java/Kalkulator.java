586b1cb77b7a6f1159a345ea204d0888d66b7393
Antek <xbbshshxhz@gmail.com>
2023-11-07 13:45:59 +0100

    Commit

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -6,20 +6,15 @@
         import java.util.ArrayList;
         import java.util.Arrays;
         import java.util.List;
+
+
         import static org.junit.jupiter.api.Assertions.assertEquals;
         import static org.junit.jupiter.api.Assertions.assertTrue;
         import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 public class Kalkulator {
     public static void main(String[] args) {
-        String str = "111";
 
-        int num1 = Integer.parseInt(str);
-        System.out.println("parseInt(String): "
-                + num1);
-
-        int num2 = Integer.parseInt(str, 2);
-        System.out.println("parseInt(String o podstawie 2): " + num2);
 ////////////////
 System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxx");
         String myString1 = "1";
@@ -30,13 +25,17 @@ System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxx");
         String myString6 = "6";
         String myString7 = "7";
         String myString8 = "8";
-        String myString9 = "9";
+        String myString9 = "9,3,4";
+        myString9 = myString9.replaceAll(",", ""); //usuwa przecinek
+        myString9 = myString9.replaceAll("\\s", ""); // usuwa spacje
+
 
 
         int parNum1 = Integer.valueOf(myString1);
         System.out.println("Integer z typu String przy użyciu valueOf(): " + parNum1);
         int parNum2 = Integer.valueOf(myString2);
-        int wynik = parNum2 + parNum1;
+        int parNum9 = Integer.valueOf(myString9);
+        int wynik = parNum2 + parNum9;
         System.out.println("Suma 1 i 2:  " + wynik);
 
     }
--- src/test/java/KalkulatorTest.java
+++ src/test/java/KalkulatorTest.java
@@ -15,16 +15,22 @@ public class KalkulatorTest {
         String myString9 = "9";
 
 
-        int parNum1 = Integer.valueOf(myString1); // konwersja String na int
-        System.out.println("Integer z typu String przy użyciu valueOf(): " + parNum1);
-        int parNum2 = Integer.valueOf(myString2); // konwersja String na int
-        int wynik = parNum2 + parNum1;
-
         int parNum1 = Integer.valueOf(myString1);
         int parNum2 = Integer.valueOf(myString2);
+        int parNum3 = Integer.valueOf(myString3);
+        int parNum4 = Integer.valueOf(myString4);
+        int parNum5 = Integer.valueOf(myString5);
+        int parNum6 = Integer.valueOf(myString6);
+        int parNum7 = Integer.valueOf(myString7);
+        int parNum8 = Integer.valueOf(myString8);
+        int parNum9 = Integer.valueOf(myString9);
+
 
         int result = parNum1 + parNum2;
 
-        Assertions.assertEquals(3, result);
+        Assertions.assertEquals(3, result);//tescik
+
+
+
     }
 }
